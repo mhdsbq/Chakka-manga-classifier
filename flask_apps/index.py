f1d5dc7d8926d319aa362a8f6c507cf3e62ac4a5
mhdsbq <18bl12313@rit.ac.in>
2021-05-20 00:47:10 +0530

    added flask

---
+++ flask_apps/index.py
@@ -0,0 +1,86 @@
+
+import os
+from keras.preprocessing.image import ImageDataGenerator
+from keras.models import Sequential
+from keras.layers import Conv2D, MaxPooling2D
+from keras.layers import Activation, Dropout, Flatten, Dense
+from keras import backend as K
+import numpy as np
+from keras.preprocessing import image
+
+
+# Flask utils
+from flask import Flask, redirect, url_for, request, render_template
+from werkzeug.utils import secure_filename
+#from gevent.pywsgi import WSGIServer
+
+
+img_width, img_height = 150, 150
+input_shape = (img_width, img_height, 3)
+
+model = Sequential()
+model.add(Conv2D(32, (3, 3), input_shape=input_shape))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+
+model.add(Conv2D(32, (3, 3)))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+model.add(Conv2D(64, (3, 3)))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+model.add(Flatten())
+model.add(Dense(64))
+model.add(Activation('relu'))
+model.add(Dropout(0.5))
+model.add(Dense(1))
+model.add(Activation('sigmoid'))
+
+
+model.load_weights('C:/Users/mhdsa/PycharmProjects/pythonProject1/first_try.h5')
+
+
+app = Flask(__name__)
+
+
+
+
+@app.route('/', methods=['GET'])
+def index():
+    # Main page
+    return render_template('index.html')
+
+
+@app.route('/predict', methods=['GET', 'POST'])
+def upload():
+    if request.method == 'POST':
+        # Get the file from post request
+        f = request.files['file']
+
+        # Save the file to ./uploads
+        basepath = os.path.dirname(__file__)
+        file_path = os.path.join(
+            basepath, 'uploads', secure_filename(f.filename))
+        f.save(file_path)
+
+        # Make prediction
+        img_pred = image.load_img(file_path, target_size=input_shape)
+        img_pred = image.img_to_array(img_pred)
+        img_pred = np.expand_dims(img_pred, axis=0)
+
+        rslt = model.predict(img_pred)
+        if rslt[0][0] == 1:
+            prediction = 'mango'
+        else:
+            prediction = 'jackfruit'
+
+        return prediction
+    return None
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
